930576ff0d862670dab78bec36656cf015073088
Tomas <tomaslq98@gmail.com>
2024-02-24 16:24:57 -0300

    add applicaiton rows

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO USERS (id, username, email, password, role) VALUES (0, 'admin', 'admin@gmail.com', 'admin1', 'admin');
+INSERT INTO "user" (id, username, email, password, role) VALUES (0, 'admin', 'admin@gmail.com', 'admin1', 'admin');
+
+INSERT INTO "APPLICATION" (id, code, name) VALUES (126, 12, 'Compras');
+INSERT INTO "APPLICATION" (id, code, name) VALUES (106, 24, 'RRHH');
+INSERT INTO "APPLICATION" (id, code, name) VALUES (211, 05, 'Licencias');

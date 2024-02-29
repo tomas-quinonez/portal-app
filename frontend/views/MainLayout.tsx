c392aca5e809a7e7c7e656c8c6bfcdb8958c1d97
Tomas <tomaslq98@gmail.com>
2024-02-29 01:37:44 -0300

    update layout

--- frontend/views/MainLayout.tsx
+++ frontend/views/MainLayout.tsx
@@ -4,57 +4,81 @@ import Placeholder from 'Frontend/components/placeholder/Placeholder.js';
 import { useRouteMetadata } from 'Frontend/util/routing.js';
 import { Suspense, useEffect } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { TestEndpointService } from "Frontend/generated/endpoints";
+import { AccordionPanel } from '@hilla/react-components/AccordionPanel.js';
+import { VerticalLayout } from '@hilla/react-components/VerticalLayout.js';
+import { Button } from '@hilla/react-components/Button.js';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import PDFDocument from 'Frontend/PDFDocument';
+import { useAuth } from 'Frontend/auth';
+
 
 const navLinkClasses = ({ isActive }: any) => {
-  return `block rounded-m p-s ${isActive ? 'bg-primary-10 text-primary' : 'text-body'}`;
+	return `block rounded-m p-s ${isActive ? 'bg-primary-10 text-primary' : 'text-body'}`;
 };
 
 export default function MainLayout() {
-  const currentTitle = useRouteMetadata()?.title ?? 'My App';
-  useEffect(() => {
-    document.title = currentTitle;
-  }, [currentTitle]);
 
-  useEffect(() => {
-    //TestEndpointService.saySomething('hola').then(m => alert(m));
-  })
+	const { state } = useAuth();
 
-  return (
-    <AppLayout primarySection="drawer">
-      <div slot="drawer" className="flex flex-col justify-between h-full p-m">
-        <header className="flex flex-col gap-m">
-          <h1 className="text-l m-0">My App</h1>
-          <nav>
-            <NavLink className={navLinkClasses} to="/">
-              Hello World
-            </NavLink>
-            <NavLink className={navLinkClasses} to="/about">
-              About
-            </NavLink>
-            <NavLink className={navLinkClasses} to="/abm/applications">
-              Aplicaciones
-            </NavLink>
-            <NavLink className={navLinkClasses} to="/abm/organisations">
-              Organimos
-            </NavLink>
-            <NavLink className={navLinkClasses} to="/abm/users">
-              Usuarios
-            </NavLink>
-            <NavLink className={navLinkClasses} to="/myapplications">
-              Mis Aplicaciones
-            </NavLink>
-          </nav>
-        </header>
-      </div>
-      <DrawerToggle slot="navbar" aria-label="Menu toggle"></DrawerToggle>
-      <h2 slot="navbar" className="text-l m-0">
-        {currentTitle}
-      </h2>
+	const currentTitle = useRouteMetadata()?.title ?? 'My App';
+	useEffect(() => {
+		document.title = currentTitle;
+	}, [currentTitle]);
 
-      <Suspense fallback={<Placeholder />}>
-        <Outlet />
-      </Suspense>
-    </AppLayout>
-  );
+
+	return (
+		<AppLayout primarySection="drawer">
+			<div slot="drawer" className="flex flex-col justify-between h-full p-m">
+				<header className="flex flex-col gap-m">
+					<h1 className="text-l m-0">Portal de Aplicaciones</h1>
+					<nav>
+						<AccordionPanel summary="Aplicaciones" theme='filled, vaadin-accordion-panel[opened]'>
+							<VerticalLayout>
+								<NavLink className={navLinkClasses} to="/">
+									Mis Aplicaciones
+								</NavLink>
+							</VerticalLayout>
+						</AccordionPanel>
+						{state.user?.authorities.includes('ROLE_ADMIN')
+							?
+							<AccordionPanel summary="Admin" theme='filled, vaadin-accordion-panel[opened]'>
+								<VerticalLayout>
+									<NavLink className={navLinkClasses} to="/abm/users">
+										A/B/M Usuarios
+									</NavLink>
+									<NavLink className={navLinkClasses} to="/abm/applications">
+										A/B/M Aplicaciones
+									</NavLink>
+									<NavLink className={navLinkClasses} to="/abm/organisations">
+										A/B/M Organismos
+									</NavLink>
+									<AccordionPanel summary="Generación PDFs" theme='filled, vaadin-accordion-panel[opened]'>
+										<VerticalLayout>
+											<PDFDownloadLink
+												document={<PDFDocument />}
+												fileName={"usuarios_autorizados_" + new Date().getTime() + ".pdf"}>
+												<Button className="px-4">
+													Usuarios Autorizados
+												</Button>
+											</PDFDownloadLink>
+										</VerticalLayout>
+									</AccordionPanel>
+								</VerticalLayout>
+							</AccordionPanel>
+							:
+							<></>
+						}
+					</nav>
+				</header>
+			</div>
+			<DrawerToggle slot="navbar" aria-label="Menu toggle"></DrawerToggle>
+			<h2 slot="navbar" className="text-l m-0">
+				{currentTitle}
+			</h2>
+
+			<Suspense fallback={<Placeholder />}>
+				<Outlet />
+			</Suspense>
+		</AppLayout>
+	);
 }
